81072b2c921e5dc69e524890b95c67ad7b3e49a1
siddhu <siddharthasanapala136@gmail.com>
2025-02-16 12:47:42 +0530

    updated the attachments - removed required

--- mail/forms.py
+++ mail/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class SendMailsForm(forms.Form):
     subject = forms.CharField(widget=forms.Textarea, max_length=100)
     message = forms.CharField(widget=forms.Textarea, max_length=500)
-    attachments = forms.FileField()
+    attachments = forms.FileField(required=False)
     excel_file = forms.FileField()
